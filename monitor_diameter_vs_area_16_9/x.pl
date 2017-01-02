22e937ef268222a2c1457871f28e7feeace5fc9e
koo5 <kolman.jindrich@gmail.com>
2017-01-02 22:41:57 +0100

    ..

--- monitor_diameter_vs_area_16_9/x.pl
+++ monitor_diameter_vs_area_16_9/x.pl
@@ -1,19 +1,16 @@
 :- [library(clpr)].
 
 tv(Diag,Area,W,H) :- {
-	pow(Diag, 2) = pow(W,2) + pow(H,2),
-	W = H * 16 / 9,
-	Area = W * H,
-	W>0,
-	H>0
+	Diag * Diag = W * W + H * H,
+	W = H * 16 / 9, Area = W * H, H>0
 	}.
 
 print_tv(D, Area, W, H) :-
 	tv(D, Area, W, H),
-	Am is Area * 2.54 / 10000,
+	Am is Area * 2.54 * 2.54 / 10000,
 	Wcm is W * 2.54,
 	Hcm is H * 2.54,
-	format('diag(inch):~0f~t~25+ area(m): ~5f~t~25+ W(cm): ~2f~t~25+ H(cm): ~2f~n', [D, Am, Wcm, Hcm]).
+	format('diag(inch):~0f~t~20+ area(m): ~5f~t~20+ W(cm): ~2f~t~20+ H(cm): ~2f~n', [D, Am, Wcm, Hcm]).
 
 
-:- forall(between(1, 200, D), print_tv(D,_,_,_)).
+:- forall(between(1, 200, W), print_tv(D,_,W,_)).
