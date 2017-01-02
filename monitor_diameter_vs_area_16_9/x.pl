622b648d75644af78c3f1bc4da57310867058c3d
koo5 <kolman.jindrich@gmail.com>
2017-01-02 22:44:42 +0100

    ..

--- monitor_diameter_vs_area_16_9/x.pl
+++ monitor_diameter_vs_area_16_9/x.pl
@@ -10,7 +10,7 @@ print_tv(D, Area, W, H) :-
 	Am is Area * 2.54 * 2.54 / 10000,
 	Wcm is W * 2.54,
 	Hcm is H * 2.54,
-	format('diag(inch):~0f~t~20+ area(m): ~5f~t~20+ W(cm): ~2f~t~20+ H(cm): ~2f~n', [D, Am, Wcm, Hcm]).
+	format('diag(inch):~1f~t~20+ area(m): ~5f~t~20+ W(cm): ~1f~t~20+ H(cm): ~1f~n', [D, Am, Wcm, Hcm]).
 
 
 :- forall(between(1, 200, W), print_tv(D,_,W,_)).
