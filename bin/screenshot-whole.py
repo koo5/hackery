40fccde993b3be5deacbed445016304d5c3ae77d
koo5 <jindrich.kolman@elektroline.cz>
2017-04-08 18:35:48 +0200

    mmm

--- bin/screenshot-whole.py
+++ bin/screenshot-whole.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python2.7
 import os, time
-while True:
-	time.sleep(2)
-	os.system("scrot  -q 80 /home/kook/screenshots/$(date +%Y%m%d%H%M%S).png")
+os.system("scrot  -q 80 /home/kook/screenshots/$(date +%Y%m%d%H%M%S).png")
